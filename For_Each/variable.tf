42948b843f3702dbc14bbbba17ea469a33557d51
VENGALAREDDY <vengalareddy.challa@gmail.com>
2025-04-04 21:32:37 +0530

    For_Each

---
+++ For_Each/variable.tf
@@ -0,0 +1,36 @@
+variable "ami_id" {
+    type=string
+    default = "ami-09c813fb71547fc4f"
+}
+variable "tags" {
+    type =map
+    default = {
+        Name = "terraform-vm"
+        project = "dev"
+    }  
+}
+variable "form_port" {
+  type = number
+  default = 0
+}
+variable "to_port" {
+  type = number
+  default = 0
+}
+variable "cidr_blocks" {
+  type = list
+  default = ["0.0.0.0/0"]
+}
+variable "instance_type" {
+  type = string
+  default = "t3.micro"
+}
+
+
+variable "instances" {
+  default = {
+    frontend = "t3.micro"
+    backend = "t3.micro"
+    database = "t3.small"
+  } 
+}
